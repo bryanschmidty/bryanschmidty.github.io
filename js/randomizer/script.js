f6b49c9a39fb828987a32de7ce70015acf0e5de9
Bryan Schmidt <bryan@fashionphile.com>
2023-06-18 21:50:17 -0600

    AI updates

--- js/randomizer/script.js
+++ js/randomizer/script.js
@@ -5,17 +5,20 @@ function displayItems() {
     itemsList.innerHTML = "";
 
     const items = JSON.parse(localStorage.getItem("items")) || [];
-    items.forEach((itemData, index) => {
+    const verbs = items.filter(item => item.parentId === null);
+
+    verbs.forEach(verb => {
         const row = document.createElement("tr");
+        row.setAttribute('data-verb-id', verb.id);
 
         const verbCell = document.createElement("td");
         verbCell.style.verticalAlign = "top";
-        renderElement(verbCell, itemData.item, itemData.id, true);
+        renderElement(verbCell, verb.name, verb.id, true);
         row.appendChild(verbCell);
 
         const objectCell = document.createElement("td");
         objectCell.style.verticalAlign = "top";
-        renderChildren(objectCell, itemData.id);
+        renderChildren(objectCell, verb.id);
         row.appendChild(objectCell);
 
         itemsList.appendChild(row);
@@ -29,196 +32,146 @@ function renderElement(parentElement, elementText, id, isVerb) {
     const element = document.createElement("div");
     element.classList.add("element");
 
-    const text = document.createElement("span");
-    text.textContent = elementText;
-    element.appendChild(text);
+    if (elementText === undefined) {
+        const addChildButton = document.createElement("button");
+        addChildButton.textContent = "Add Child";
+        addChildButton.addEventListener("click", () => {
+            addChildButton.remove();
+            editChild(element, "", null);
+        });
+        element.appendChild(addChildButton);
+        elementWrapper.appendChild(element);
+    } else {
+        const text = document.createElement("span");
+        text.textContent = elementText;
+        element.appendChild(text);
+        elementWrapper.appendChild(element);
 
-    elementWrapper.appendChild(element);
+        const editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.addEventListener("click", () => {
+            editChild(element, elementText, id);
+        });
+        elementWrapper.appendChild(editButton);
 
-    const editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    editButton.addEventListener("click", () => {
-        editElement(element, elementText, id, isVerb);
-    });
-    elementWrapper.appendChild(editButton);
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", () => {
+            deleteElement(id, isVerb);
+        });
+        elementWrapper.appendChild(deleteButton);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", () => {
-        deleteElement(id, isVerb);
-    });
-    elementWrapper.appendChild(deleteButton);
-
+    }
     parentElement.appendChild(elementWrapper);
 }
 
 function renderChildren(parentElement, verbId) {
     const items = JSON.parse(localStorage.getItem("items")) || [];
-    const itemData = items.find(item => item.id === verbId);
+    const children = items.filter(item => item.parentId == verbId);
+    console.log(verbId, children)
 
     parentElement.innerHTML = "";
-    itemData.children.forEach((child, childIndex) => {
-        renderElement(parentElement, child.text, { verbId, childIndex }, false);
+    children.forEach(child => {
+        console.log(child)
+        renderElement(parentElement, child.name, child.id, false);
     });
 
-    const addChildButton = document.createElement("button");
-    addChildButton.textContent = "Add Child";
-    addChildButton.addEventListener("click", () => {
-        const childIndex = addChild(verbId);
-        editChild(parentElement, "", { verbId, childIndex });
-    });
-    parentElement.appendChild(addChildButton);
+    renderElement(parentElement);
 }
 
-function editVerb(element, elementText, id) {
-    const parentElement = element.parentElement;
-    parentElement.innerHTML = "";
+function editChild(element, elementText, id) {
+    let parentId = element.closest('tr').getAttribute('data-verb-id');
+    let siblings = Array.from(element.parentNode.children);
+    for(let i = 0; i < siblings.length; i++){
+        if(siblings[i] !== element) {
+            siblings[i].remove();
+        }
+    }
+
+    element.innerHTML = ""
 
     const input = document.createElement("input");
     input.type = "text";
     input.value = elementText;
-    parentElement.appendChild(input);
+    element.appendChild(input);
 
     const saveButton = document.createElement("button");
     saveButton.textContent = "Save";
     saveButton.addEventListener("click", () => {
+        const items = JSON.parse(localStorage.getItem("items")) || [];
         if (id === null) {
-            addVerb(input.value);
+            id = Date.now() + Math.random();
+            items.push({ id, name: input.value, parentId });
         } else {
-            updateVerb(id, input.value);
+            const itemIndex = items.findIndex(item => item.id === id);
+            items[itemIndex].name = input.value;
         }
-        displayItems(); // Re-render everything after saving
+        localStorage.setItem("items", JSON.stringify(items));
+        displayItems();
     });
-    parentElement.appendChild(saveButton);
-}
+    element.appendChild(saveButton);
 
-function editChild(parentElement, elementText, id) {
-    const input = document.createElement("input");
-    input.type = "text";
-    input.value = elementText;
-    parentElement.insertBefore(input, parentElement.lastChild);
+    // Put the cursor in the input box at the end of the text
+    input.focus();
+    input.setSelectionRange(input.value.length, input.value.length);
 
-    const saveButton = document.createElement("button");
-    saveButton.textContent = "Save";
-    saveButton.addEventListener("click", () => {
-        updateChild(id.verbId, id.childIndex, input.value);
-        displayItems(); // Re-render everything after saving
-    });
-    parentElement.insertBefore(saveButton, parentElement.lastChild);
-}
-
-function editElement(element, elementText, id, isVerb) {
-    const parentElement = element.parentElement;
-    parentElement.innerHTML = "";
-
-    const input = document.createElement("input");
-    input.type = "text";
-    input.value = elementText;
-    parentElement.appendChild(input);
-
-    const saveButton = document.createElement("button");
-    saveButton.textContent = "Save";
-    saveButton.addEventListener("click", () => {
-        if (isVerb && id === null) {
-            addVerb(input.value);
-        } else {
-            updateElement(input, id, isVerb);
+    // Add event listener for the Enter key on the input box
+    input.addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) { // 13 is the keyCode for the Enter key
+            event.preventDefault(); // Prevent the default action (form submission in this case)
+            saveButton.click(); // Simulate a click on the save button
         }
-        displayItems(); // Re-render everything after saving
     });
-    parentElement.appendChild(saveButton);
 }
 
-function updateElement(input, id, isVerb) {
+
+function updateElement(input, id) {
     const newValue = input.value;
     if (isVerb) {
         updateVerb(id, newValue);
     } else {
-        updateChild(id.itemIndex, id.childIndex, newValue);
+        updateChild(id, newValue, verbId);
     }
 }
 
 function updateVerb(id, newValue) {
     const items = JSON.parse(localStorage.getItem("items")) || [];
     const itemIndex = items.findIndex(item => item.id === id);
-    items[itemIndex].item = newValue;
+    items[itemIndex].name = newValue;
     localStorage.setItem("items", JSON.stringify(items));
     displayItems();
 }
 
-function updateChild(childId, newValue) {
+function updateChild(id, newValue, verbId) {
     const items = JSON.parse(localStorage.getItem("items")) || [];
-    for (const item of items) {
-        const childIndex = item.children.findIndex(child => child.id === childId);
-        if (childIndex !== -1) {
-            item.children[childIndex].text = newValue;
-            break;
-        }
+
+    if (id === null) {
+        id = addChild(verbId);
     }
+
+    const childIndex = items.findIndex(item => item.id === id);
+    items[childIndex].name = newValue;
+
     localStorage.setItem("items", JSON.stringify(items));
     displayItems();
 }
 
 function deleteElement(id, isVerb) {
-    if (isVerb) {
-        deleteVerb(id);
-    } else {
-        const items = JSON.parse(localStorage.getItem("items")) || [];
-        let verbIndex, childIndex;
-        for (let i = 0; i < items.length; i++) {
-            const idx = items[i].children.findIndex(child => child.id === id);
-            if (idx !== -1) {
-                verbIndex = i;
-                childIndex = idx;
-                break;
-            }
-        }
-        deleteChild(verbIndex, childIndex);
+    const items = JSON.parse(localStorage.getItem("items")) || [];
+    const itemIndex = items.findIndex(item => item.id === id);
+    if (itemIndex !== -1) {
+        items.splice(itemIndex, 1);
+        localStorage.setItem("items", JSON.stringify(items));
     }
-}
-
-function deleteVerb(index) {
-    const items = JSON.parse(localStorage.getItem("items")) || [];
-    items.splice(index, 1);
-    localStorage.setItem("items", JSON.stringify(items));
     displayItems();
-}
-
-function deleteChild(verbIndex, childIndex) {
-    const items = JSON.parse(localStorage.getItem("items")) || [];
-    items[verbIndex].children.splice(childIndex, 1);
-    localStorage.setItem("items", JSON.stringify(items));
-    displayItems();
-}
-
-
-function saveButtonHandler(childElement, childInput, verbIndex, childIndex) {
-    const newChild = childInput.value;
-    updateChild(verbIndex, childIndex, newChild);
-    renderChildren(childElement.parentElement, verbIndex);
 }
 
 function addChild(verbId) {
     const items = JSON.parse(localStorage.getItem("items")) || [];
-    const itemIndex = items.findIndex(item => item.id === verbId);
     const childId = Date.now() + Math.random();
-    items[itemIndex].children.push({ id: childId, text: "" });
+    items.push({ id: childId, name: "", parentId: verbId });
     localStorage.setItem("items", JSON.stringify(items));
     return childId;
-}
-
-function addObject(verbIndex, object) {
-    const items = JSON.parse(localStorage.getItem("items")) || [];
-    items[verbIndex].children.push(object);
-    localStorage.setItem("items", JSON.stringify(items));
-    displayItems();
-}
-
-function deleteItem(verbIndex, childIndex) {
-    const items = JSON.parse(localStorage.getItem("items")) || [];
-    items[verbIndex].children.splice(childIndex, 1);
-    localStorage.setItem("items", JSON.stringify(items));
-    displayItems();
 }
 
 document.getElementById("add-verb").addEventListener("click", () => {
@@ -232,15 +185,14 @@ document.getElementById("add-verb").addEventListener("click", () => {
 
     itemsList.appendChild(newRow);
 
-    editVerb(verbCell, "", null);
+    editChild(verbCell, "", null);
 });
 
 function addVerb(verb) {
     const items = JSON.parse(localStorage.getItem("items")) || [];
-    const verbId = Date.now(); // Use a timestamp as a unique identifier
-    items.push({ id: verbId, item: verb, children: [] });
+    const verbId = Date.now() + Math.random();
+    items.push({ id: verbId, name: verb, parentId: null });
     localStorage.setItem("items", JSON.stringify(items));
-    displayItems();
     return verbId;
 }
 
